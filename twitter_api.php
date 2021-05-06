b88ded11c4245dce2d6c7bd051b6681531092229
kixxp <83739739+kixxp@users.noreply.github.com>
2021-05-07 00:32:18 +0200

    Update twitter_api.php

--- twitter_api.php
+++ twitter_api.php
@@ -1,4 +1,18 @@
 <?php
+require_once('TwitterAPIExchange.php');
+
+$settings = array(
+    'oauth_access_token' => "2707638549-J4v0HGZ1CU84ali7vJwEDVqclbKSKSIA0t660te",
+    'oauth_access_token_secret' => "YdhXskDvXVa7nd9abtcfKcmRAoUl84D3nRDDK6U2ASzLkQ",
+    'consumer_key' => "VtN5XZtSbUnZyPRM2lulNhSL6",
+    'consumer_secret' => "YKflVrwa5soGyoFiNQuDlGr9BRZ03xGdkQF7Dj6URl2hgU7zDY"
+);
+
+$url = 'https://api.twitter.com/1.1/followers/ids.json';
+$requestMethod = 'POST'; 
+
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod);
 
 ?>
 
