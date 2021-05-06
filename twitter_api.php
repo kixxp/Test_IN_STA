f920cedc53b6ea4bf4e303a7453435e8ac49efa3
kixxp <83739739+kixxp@users.noreply.github.com>
2021-05-06 23:05:18 +0200

    Update twitter_api.php

--- twitter_api.php
+++ twitter_api.php
@@ -3,7 +3,7 @@ require_once('TwitterAPIExchange.php');
 
 $settings = array(
     'oauth_access_token' => "2707638549-J4v0HGZ1CU84ali7vJwEDVqclbKSKSIA0t660te",
-    'oauth_access_token_secret' => "YdhXskDvXVa7nd9abtcfKcmRAoUl84D3nRDDK6U2ASzLkQ",
+    'oauth_access_token_secret' => "dhXskDvXVa7nd9abtcfKcmRAoUl84D3nRDDK6U2ASzLkQ",
     'consumer_key' => "VtN5XZtSbUnZyPRM2lulNhSL6",
     'consumer_secret' => "YKflVrwa5soGyoFiNQuDlGr9BRZ03xGdkQF7Dj6URl2hgU7zDY"
 );
