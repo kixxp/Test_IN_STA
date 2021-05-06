06c82fdec5d5ceaf90c72e47ccb78724e02b5c60
kixxp <83739739+kixxp@users.noreply.github.com>
2021-05-07 00:30:31 +0200

    Update twitter_api.php

--- twitter_api.php
+++ twitter_api.php
@@ -24,6 +24,7 @@ echo $twitter->buildOauth($url, $requestMethod);
     <link rel="stylesheet" type="text/css" href="" />
 </head>
 <body>
+    <h1>Startseite</h1>
     <blockquote class="twitter-tweet" data-lang="en">
     <p lang="de" dir="ltr">Wir sind doch nur Marionetten der Politik, die uns von vorne bis hinten fickt.</p>&mdash; Kristijan Pehar (@KikiPehar) <a href="https://twitter.com/KikiPehar/status/1017543367977299968">13. Juli 2018</a>
     </blockquote>
