f29b8adb5b936bf7442306047aba124b767ba516
kixxp <83739739+kixxp@users.noreply.github.com>
2021-05-07 05:29:53 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,3 @@
 <?php
 $test = 1;
 ?>
-
-<!DOCTYPE html>
-<html lang="de">
-<head>
-  <meta charset="UTF-8">
-  <title>Test</title>
-  <link rel="stylesheet" type="text/css" href="" />
-</head>
-<body>
-  <h1>Test</h1>
-  <div>
-    <?php if($test<=1) : ?>
-      <p> Test erfolgreich </p>
-    <?php endif; ?>
-  </div>
-</body>
-</html>
