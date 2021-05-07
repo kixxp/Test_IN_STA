3d8b70579c22ff1ce9e4f9c733ff329dd52b0029
kixxp <83739739+kixxp@users.noreply.github.com>
2021-05-07 05:22:00 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+$titel = "Startseite";
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <title>Test</title>
+  <link rel="stylesheet" type="text/css" href="" />
+</head>
+<body>
+  <h1><?php $titel; ?></h1>
+</body>
+</html>
